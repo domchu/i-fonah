3a79ad923469a469ee766a88a8cd4146c02e4216
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-12-10 19:16:58 +0100

    clean setup

---
+++ component/FaqPage/FaqPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FaqPage = () => {
+  return (
+    <div>
+      <h1>FaqPage</h1>
+    </div>
+  );
+};
+
+export default FaqPage;
---
+++ component/data.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  FaWhatsApp,
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+export const social = [
+  {
+    id: 1,
+    url: "https://www.twitter.com",
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: "https://www.twitter.com",
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: "https://www.twitter.com",
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 4,
+    url: "https://www.twitter.com",
+    icon: <FaWhatsApp />,
+  },
+  {
+    id: 5,
+    url: "https://www.twitter.com",
+    icon: <FaInstagram />,
+  },
+];
