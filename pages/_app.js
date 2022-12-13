c6d4f6d3ebdb013db8f716c9eff6cde6d34db12d
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-12-13 01:46:53 +0100

    chore: navlink

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
-import "../styles/globals.css";
+// import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
+import Navbar from "../component/Container/Navbar";
+import Footer from "./../component/Container/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* <Navbar /> */}
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Navbar from "./../component/Container/Navbar";
-import Contact from "./contact";
 
 export default function Home() {
   return (
