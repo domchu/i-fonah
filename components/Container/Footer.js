383d00ea87cdc8a0929c809ae5d6f697a8bcf89f
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-12-15 00:45:26 +0100

    footer:added the all right reserved

--- components/Container/Footer.js
+++ components/Container/Footer.js
@@ -4,10 +4,23 @@ const Footer = () => {
   return (
     <>
       <div>
-        <div></div>
         <div>
-          <p>i-fonah &copy;2022 All Right Reserved</p>
+          <h1>i-fonah</h1>
         </div>
+        <div>
+          <h1>Main Office</h1>
+          <div>
+            <p>25 Allen Avenue, Ikeja. Lagos state</p>
+            <h3>Contact: +234(0) 703 4278 995</h3>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="footer">
+        <p className="h-20 text-2xl text-black flex justify-center items-center">
+          i-fonah &copy;2022 All Right Reserved
+        </p>
       </div>
     </>
   );
