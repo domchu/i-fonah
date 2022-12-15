da7ad00e1a1e684105fbf385c18889612ba05568
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-12-15 02:09:08 +0100

    styles: reduce the font size of the text

--- components/Container/Footer.js
+++ components/Container/Footer.js
@@ -3,48 +3,48 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <>
-      <div className="flex justify-evenly p-8 text-white bg-black  text-2xl">
+      <div className="flex justify-evenly p-8 text-white bg-black  text-2xl ">
         <div>
           <h1 className="py-4 text-3xl">i-fonah</h1>
         </div>
         <div>
           <h1 className="py-4 text-3xl">Main Office</h1>
           <div>
-            <p className=" py-2">
-              Address: 25 Allen Avenue, Ikeja. Lagos state
+            <p className="py-2 text-xs">
+              Address : 25 Allen Avenue, Ikeja. Lagos state
             </p>
-            <h3>Contact: +234(0) 703 4278 995</h3>
+            <h3 className="text-xs">Contact : +234(0) 703 4278 995</h3>
           </div>
         </div>
         <div>
           <h1 className="py-4 text-3xl">Useful Links</h1>
           <ul>
-            <li className="hover:text-orange-200 py-2">
+            <li className="hover:text-orange-200 py-2 text-xs">
               <Link href="/" className="un-list" passHref>
                 Our Services
               </Link>
             </li>
-            <li className="hover:text-orange-200 py-2">
+            <li className="hover:text-orange-200 py-2 text-xs">
               <Link href="/" className="un-list" passHref>
                 Our Team
               </Link>
             </li>
-            <li className="hover:text-orange-200 py-2">
+            <li className="hover:text-orange-200 py-2 text-xs">
               <Link href="/" className="un-list" passHref>
                 Portfolio
               </Link>
             </li>
-            <li className="hover:text-orange-200 py-2">
+            <li className="hover:text-orange-200 py-2 text-xs">
               <Link href="/" className="un-list" passHref>
                 Short Codes
               </Link>
             </li>
-            <li className="hover:text-orange-200 py-2">
+            <li className="hover:text-orange-200 py-2 text-xs">
               <Link href="/" className="un-list" passHref>
                 Topography
               </Link>
             </li>
-            <li className="hover:text-orange-200 py-2">
+            <li className="hover:text-orange-200 py-2 text-xs">
               <Link href="/" className="un-list" passHref>
                 Privacy policy
               </Link>
@@ -56,7 +56,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="footer">
-        <p className="h-20 text-2xl text-black flex justify-center items-center">
+        <p className="h-20 text-xl text-black flex justify-center items-center">
           i-fonah &copy;2022 All Right Reserved
         </p>
       </div>
