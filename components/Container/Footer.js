85c98a891025dde0a9d01d7230ee3c98bcffecbf
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-12-16 14:47:32 +0100

    styles: small screen styles

--- components/Container/Footer.js
+++ components/Container/Footer.js
@@ -4,16 +4,14 @@ import SocialData from "../SocialData";
 const Footer = () => {
   return (
     <>
-      <div className="md:flex justify-evenly p-8 text-white bg-black text-2xl">
+      <div className=" block md:flex justify-evenly p-8 text-white bg-black text-2xl">
         <div>
-          <h1 className="py-4 text-3xl">
-            TwitchTech (T<sup>2</sup>){" "}
-          </h1>
+          <h1 className="py-4 text-3xl">IFONTECH</h1>
         </div>
         <div>
           <h1 className="py-4 text-3xl">Main Office</h1>
           <div>
-            <div className="flex">
+            <div className="flex ">
               <div>
                 <h4 className="text-orange-400">Address </h4>
               </div>
@@ -23,7 +21,7 @@ const Footer = () => {
                 </p>
               </div>
             </div>
-            <div className="flex py-2">
+            <div className="flex py-2 ">
               <div>
                 <h4 className="text-orange-400">Contact </h4>
               </div>
@@ -69,26 +67,23 @@ const Footer = () => {
           </ul>
         </div>
         <div>
-          <h1 className="py-4 text-3xl">Social Media</h1>
-          //* mapping the social media icons
-          {SocialData.map((item, index) => {
+          <h1 className="py-3 text-3xl">Social Media</h1>
+
+          {/* {SocialData.map((item, index) => {
             const { url, icon } = item;
             return (
               <li key={index}>
                 <Link to={url}>{icon}</Link>
               </li>
             );
-          })}
-          //* end of the social media mapped
+          })} */}
         </div>
       </div>
       <div className="underline"></div>
       <div className="footer">
-        <p className="footer-text">
-          TwitchTech (T<sup>2</sup>) &copy;2022, All Right Reserved
-        </p>
+        <p className="footer-text">IfonTech &copy;2022, All Right Reserved</p>
       </div>
-      <SocialData />
+      {/* <SocialData /> */}
     </>
   );
 };
