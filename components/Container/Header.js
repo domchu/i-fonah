3ed0be3205a9f0ea48f3ad798d1dfb6a066c8290
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-12-18 00:58:14 +0100

    bug:remove unclean code

--- components/Container/Header.js
+++ components/Container/Header.js
@@ -1,11 +1,61 @@
 import React from "react";
+import {
+  FaWhatsapp,
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div className="h-20 md:h-20 bg-gray-200 text-black left-0 top-0 w-full ease-in duration-300">
-      <div className="md: flex justify-around">
-        <div></div>
-        <div></div>
+    <div className="md:h-20 bg-gray-200 text-black left-0 top-0 w-full">
+      {/* contact section of the project*/}
+      <div className="hidden md:flex justify-around">
+        <div className="flex">
+          <div className="flex items-center">
+            <FaFacebook />
+            <p className="text-xm">+234(0) 703 4278 995</p>
+          </div>
+          <div className="flex items-center">
+            <FaFacebook />
+            <p className="text-xm">+234(0) 703 4278 995</p>
+          </div>
+          <div className="flex items-center">
+            <FaFacebook />
+            <p className="text-xm"> 25 Allen Avenue, Ikeja. Lagos state</p>
+          </div>
+        </div>
+        {/* icons section of the project */}
+        <div>
+          <ul className="flex">
+            <li className="hover:text-orange-400 hover:animate-bounce text-xl">
+              <a href="https://www.twitter.com">
+                <FaLinkedin />
+              </a>
+            </li>
+            <li className="hover:text-orange-400 hover:animate-bounce text-xl ">
+              <a href="https://www.twitter.com">
+                <FaFacebook />
+              </a>
+            </li>
+            <li className="hover:text-orange-400 hover:animate-bounce text-xl">
+              <a href="https://www.twitter.com">
+                <FaTwitter />
+              </a>
+            </li>
+            <li className="hover:text-orange-400 hover:animate-bounce text-xl">
+              <a href="https://www.twitter.com">
+                <FaWhatsapp />
+              </a>
+            </li>
+            <li className=" hover:text-orange-400 hover:animate-spin text-xl">
+              <a href="https://www.twitter.com">
+                <FaInstagram />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
