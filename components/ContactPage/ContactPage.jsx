1945fa2398df0762c4e6ca3306b1502b59f62dcd
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-12-20 23:30:16 +0100

    chore:added contact text

--- components/ContactPage/ContactPage.jsx
+++ components/ContactPage/ContactPage.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import Link from "next/link";
 
 const ContactPage = () => {
   return (
-    <div>
-      <h1>ContactPage</h1>
-    </div>
+    <>
+      <div>
+        <div className="contact-banner">
+          <h1>CONTACT US</h1>
+
+          <Link href="/" passHref>
+            HOME
+          </Link>
+          <Link href="/" passHref>
+            MAKE A CALL
+          </Link>
+        </div>
+      </div>
+    </>
   );
 };
 
