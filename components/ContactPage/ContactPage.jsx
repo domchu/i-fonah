fb39917e49608b043dcf981e923511e214aae6d4
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-12-21 00:08:30 +0100

    added the contact banner img

--- components/ContactPage/ContactPage.jsx
+++ components/ContactPage/ContactPage.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Link from "next/link";
+import banner from "../../public/images/contact2.jpg";
 
 const ContactPage = () => {
   return (
     <>
       <div>
-        <div className="contact-banner">
+        <div
+          className="contact-banner"
+          style={{
+            backgroundImage: `url(${banner.src})`,
+            width: "100%",
+            height: "70vh",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
           <h1>CONTACT US</h1>
 
           <Link href="/" passHref>
