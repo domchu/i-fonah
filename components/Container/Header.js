b0488c5531fb6dc3e533e982b4f9aa72859d2f34
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-12-20 22:26:37 +0100

    chore:mapped the social icons to the header

--- components/Container/Header.js
+++ components/Container/Header.js
@@ -9,14 +9,16 @@ import {
 } from "react-icons/fa";
 import { FiPhoneCall } from "react-icons/fi";
 import { GiTimeBomb } from "react-icons/gi";
+import socialIcons from "../SocialData";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <>
-      <div className="md:h-14 bg-gray-200 text-black left-0 top-0 w-full">
+      <div className="md:h-14 bg-black text-white left-0 top-0 w-full">
         {/* contact section of the project*/}
         <div className="hidden md:flex justify-around">
-          <div className="flex items-center text-gray-600 mt-5">
+          <div className="flex items-center text-white-600 mt-5">
             <div className="flex items-center">
               <GiTimeBomb className="text-xl" />
               <p className="text-xm pl-2">
@@ -35,36 +37,29 @@ const Header = () => {
             </div>
           </div>
           {/* icons section of the project */}
-          <div>
-            <ul className="flex items-center text-gray-600 mt-5">
-              <li className="hover:text-orange-400 hover:animate-bounce text-xl pr-3">
-                <a href="https://www.twitter.com">
-                  <FaLinkedin />
-                </a>
-                {/* <div className="h-10 w4 text-black-400"></div> */}
-              </li>
-              <li className="hover:text-orange-400 hover:animate-bounce text-xl pr-3">
-                <a href="https://www.twitter.com">
-                  <FaFacebook />
-                </a>
-              </li>
-              <li className="hover:text-orange-400 hover:animate-bounce text-xl pr-3">
-                <a href="https://www.twitter.com">
-                  <FaTwitter />
-                </a>
-              </li>
-              <li className="hover:text-orange-400 hover:animate-bounce text-xl pr-3">
-                <a href="https://www.twitter.com">
-                  <FaWhatsapp />
-                </a>
-              </li>
-              <li className=" hover:text-orange-400 hover:animate-spin text-xl">
-                <a href="https://www.twitter.com">
-                  <FaInstagram />
-                </a>
-              </li>
-            </ul>
+
+          {/* the icons section of the project */}
+
+          <div className="flex items-center text-white mt-5">
+            {socialIcons.map((item, index) => {
+              const { url, icon } = item;
+              return (
+                <>
+                  <ul>
+                    <li
+                      key={index}
+                      className="hover:text-orange-400 hover:animate-bounce text-xl pr-3"
+                    >
+                      <Link href={url} passHref>
+                        {icon}
+                      </Link>
+                    </li>
+                  </ul>
+                </>
+              );
+            })}
           </div>
+          {/* the icons section of the project */}
         </div>
       </div>
     </>
