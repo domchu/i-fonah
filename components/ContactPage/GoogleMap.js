943000fdf257af9e32b54c01de0a275874c885a0
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-12-23 00:32:46 +0100

    chore:added the vertical line

--- components/ContactPage/GoogleMap.js
+++ components/ContactPage/GoogleMap.js
@@ -14,6 +14,9 @@ const GoogleMap = () => {
           We Help industries and organizations turn ideas into creativity and
           funds
         </h2>
+        {/* vertical line */}
+        <div className="hidden md:hidden lg:bg-gray-300 lg:flex lg:items-center lg:justify-center lg:h-24 lg:w-0.5 lg:m-auto"></div>
+        {/* vertical line */}
         <div className="flex justify-center items-center md:w-3/5 ">
           <FiPhoneCall className="mr-4 text-3xl" />
           <div className="text-xl lg:text-2xl">
@@ -21,6 +24,9 @@ const GoogleMap = () => {
             <p>+234 07034278995</p>
           </div>
         </div>
+        {/* vertical line */}
+        <div className="hidden md:hidden lg:bg-gray-300 lg:flex lg:items-center lg:justify-center lg:h-24 lg:w-0.5 lg:m-auto"></div>
+        {/* vertical line */}
         <div className=" grid place-items-center md:w-3/5">
           <li className="flex justify-center text-center rounded-md items-center mt-8 mb-6 p-4 border-x-2 border-y-2 bg-orange-500 hover:bg-orange-300 hover:text-black hover:animate-bounce">
             <a href="tel:+234 7034278995">
