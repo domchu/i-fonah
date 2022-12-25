dd5529df14e327a1d11c9059cb2b7ba59323fb77
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-12-25 19:00:16 +0100

    chore: added the newsletter components

--- components/ContactPage/GoogleMap.js
+++ components/ContactPage/GoogleMap.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FiPhoneCall } from "react-icons/fi";
 import { BsArrowRightCircle } from "react-icons/bs";
-
+import NewsLetter from "./NewsLetter";
 
 const GoogleMap = () => {
   return (
@@ -36,30 +36,7 @@ const GoogleMap = () => {
           </li>
         </div>
       </div>
-
-      {/* new letter part of the project */}
-      <div className="wrap md:flex md:justify-around bg-gray-300 text-black md:h-32 md:items-center md:px-14 capitalize">
-        <div>
-          <h1 className="text-2xl w-full text-center p-4 md:text-3xl">
-            Newsletter subscription Get the latest deal from TwitchTech
-          </h1>
-        </div>
-        <div className="flex justify-center items-center text-center py-4">
-          <input
-            type="text"
-            className="border-x-2 border-y-2 h-8 text-center py-7 text-xl cursor-pointer"
-            placeholder="Enter Email ......."
-          />
-        </div>
-        <div className=" flex justify-center items-center text-center py-4">
-          <input
-            type="submit"
-            value="SUBSCRIBE"
-            placeholder="Subscribe"
-            className="bg-black text-white py-5 px-14 mb-4 text-xl rounded-md cursor-pointer hover:bg-orange-300 hover:text-black hover:animate-bounce"
-          />
-        </div>
-      </div>
+      <NewsLetter />
     </>
   );
 };
