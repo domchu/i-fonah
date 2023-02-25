33639cbfdbf2376f1a4f1d320b9b1743b71446ab
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-26 00:16:54 +0100

    chores:added the about texts

--- components/Home/AboutUs.js
+++ components/Home/AboutUs.js
@@ -3,8 +3,13 @@ import react from "react";
 const AboutUs = () => {
   return (
     <>
-      <div className="title">
-        <h2>ABOUT US</h2>
+      <div classNmae="about-us">
+        <div className="title">
+          <h2>ABOUT US</h2>
+        </div>
+        <div>
+          <h1>We're Delivering Only exceptional Qualify Work</h1>
+        </div>
       </div>
     </>
   );
