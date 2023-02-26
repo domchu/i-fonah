c30e1838943b2c25e5eafd07dac248cb23965368
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-26 23:24:03 +0100

    chores:added the images to tghe home page

--- components/Home/AboutUs.js
+++ components/Home/AboutUs.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+import contact2 from "../../public/images/contact2.jpg";
 
 const AboutUs = () => {
   return (
@@ -9,6 +11,11 @@ const AboutUs = () => {
         </div>
         <div>
           <h1>We're Delivering Only exceptional Qualify Work</h1>
+        </div>
+        <div className="about-us-container">
+          <div>
+            <Image src={contact2} alt="about us" width={500} height={450} />
+          </div>
         </div>
       </div>
     </>
